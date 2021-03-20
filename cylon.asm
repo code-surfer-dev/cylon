4c63f01b024dbeebe2ab5eb94867c29660d3217c
Ingo Schmied <ingo@ingoneering.com>
2021-03-20 10:19:27 -0700

    update to use Z80 PIO instead of 8255

--- cylon.asm
+++ cylon.asm
@@ -6,28 +6,28 @@
 ;---------------------------------------------------------------------------
 
 	.CR	Z80				;It's a Z80 assembler now
-	.TF cylon.bin, bin
+	.TF /Users/ingoschmied/Documents/develop.nosync/assembly/cylon/cylon.bin, bin
 	.OR $0000
 
-PORT_A		.EQ $00		;8255 PORT A address - LED bargraph
-PORT_B		.EQ $01		;8255 PORT B address - Not used
-PORT_C		.EQ $02		;8255 PORT C address - Not used
-PORT_CTL	.EQ $03		;8255 Control register address
+A_DAT		.EQ $00		;PIO PORT A data
+B_DAT		.EQ $01		;PIO PORT B data
+A_CTL		.EQ $02		;PIO PORT A control
+B_CTL   	.EQ $03		;PIO PORT B control
 
-    LD A, $80           ;Set all 8255 ports to outputs
-    OUT (PORT_CTL), A
+    LD A, $0F           ;Set PIO PORT A to output
+    OUT (A_CTL), A
 
     LD B, $80           ;Set the highest bit to bit 7
 RESET
     LD A, $01           ;Start at bit 0
-    OUT (PORT_A), A
+    OUT (A_DAT), A
 MOVE_LEFT
     SLA A
-    OUT (PORT_A), A
+    OUT (A_DAT), A
     CP B
     JP NZ,MOVE_LEFT
 MOVE_RIGHT
     SRL A
     JP Z,RESET
-    OUT (PORT_A), A
+    OUT (A_DAT), A
     JP MOVE_RIGHT
